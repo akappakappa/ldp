5fae2d5f9e3894bb9f5b0e9d14fdfe6ef25c9c4c
akappakappa <20224688+akappakappa@users.noreply.github.com>
2020-12-22 16:57:37 +0100

    lab05 es03 Controller : updated with enum

--- lab05/03_Controller.cpp
+++ lab05/03_Controller.cpp
@@ -1,63 +1,90 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-/*struct Point { int x, y; };
-class Shape
-{
-public:
-    void draw() const;
-    virtual void move(int dx, int dy);
-    Point point(int i) const; // accesso ai punti
-    int number_of_points() const;
-    Shape(const Shape&) = delete;
-    Shape& operator=(const Shape&) = delete;
-    virtual ~Shape() {}
-protected:
-    Shape() {}
-    Shape(initializer_list<Point> lst);
-    virtual void draw_lines() const; // disegno
-    void add(Point p); // aggiunge un punto
-    void set_point(int i, Point p); // points[i] = p;
-private:
-    vector<Point> points; // non usato da tutte le Shape
-    Color lcolor; // dalla libreria grafica
-    Line_style ls; // dalla libreria grafica
-    Color fcolor; // dalla libreria grafica
-};*/
 
 class Controller
 {
-protected:
-    bool status;
-    int level;
 public:
-    Controller() : status{true}, level{0} {};
-    virtual void on();
-    virtual void off();
-    virtual void set_level(int l);
-    virtual void show();
+    virtual void on() = 0;
+    virtual void off() = 0;
+    virtual void set_level(int l) = 0;
+    virtual void show() = 0;
 };
 
 class Controller_test : public Controller
 {
+private:
+    bool status;
+    int level;
 public:
-    Controller_test() : Controller() {}
-
+    Controller_test() : status{true}, level{0} {}
     void on() override { status = true; }
     void off() override { status = false; }
-    void set_level(int l) override { level = l; }
+    void set_level(int l) override
+    {
+        if (!status)
+            return;
+        level = l;
+    }
     void show() override { cout << "> status: " << status << " level: " << level << endl; }
 };
 
 class Shape_color_controller : public Controller
 {
+private:
+	bool status;
+    enum Color { white, red, green, blue, black };
+    Color level;
 public:
-    Shape_color_controller(/* args */);
+    Shape_color_controller() : status{true}, level{(Color)0} {}
+    void on() override { status = true; }
+	void off() override { status = false; }
+	void set_level(int l)
+    {
+        if (!status)
+            return;
+        if (l >= 0 || l <= 4)
+            level = (Color)l;
+        else
+            cout << "level not associated with color" << endl;
+    }
+	void show() override
+    {
+        string c;
+        switch (level)
+        {
+            case 0: c = "white"; break;
+            case 1: c = "red"; break;
+            case 2: c = "green"; break;
+            case 3: c = "blue"; break;
+            case 4: c = "black"; break;
+        }
+        cout << "> status: " << status << " level: " << level << " color: " << c << endl;
+    }
 };
 
 int main()
 {
-    Controller c;
+    Controller_test c1;
+    c1.off();
+    c1.set_level(2);
+    c1.show();
+    c1.on();
+    c1.set_level(2);
+    c1.show();
+
+    Shape_color_controller c2;
+    c2.off();
+    c2.set_level(2);
+    c2.show();
+    c2.on();
+    c2.set_level(1);
+    c2.show();
+    c2.set_level(2);
+    c2.show();
+    c2.set_level(3);
+    c2.show();
+    c2.set_level(4);
+    c2.show();
+
     return 0;
 }
